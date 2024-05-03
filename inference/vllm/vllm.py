fba236340ffd25d5ceabd27150fa77f84a121bab
Darren Angle <darrendarrenangle@gmail.com>
2024-05-02 19:52:31 -0500

    feat: 🎸 add inference command that works

--- inference/vllm/vllm.py
+++ inference/vllm/vllm.py
@@ -16,29 +16,31 @@ def start_server(model_path=None):
         "--kv-cache-dtype", "fp8_e5m2",
         '--tensor-parallel-size', '2',
         "--enforce-eager",
-        "--port", "5000"
+        "--port", "5001"
     ]
     return subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, preexec_fn=os.setsid)
 
 def read_output(process):
     error_pattern = re.compile(r"AsyncEngineDeadError: Task finished unexpectedly\.")
-    while True:
-        ready, _, _ = select.select([process.stdout, sys.stdin], [], [])
-        if process.stdout in ready:
-            output = process.stdout.readline()
-            if output == '' and process.poll() is not None:
-                break
-            if output:
-                print(output.strip())
-                if error_pattern.search(output):
-                    print("Error detected. Restarting the server...")
-                    terminate_process(process)
-                    return True
-        if sys.stdin in ready:
-            input()  # Consume the input to prevent blocking
-            print("Keyboard interrupt received. Terminating the server...")
-            terminate_process(process)
-            return False
+    try:
+        while True:
+            ready, _, _ = select.select([process.stdout, sys.stdin], [], [])
+            if process.stdout in ready:
+                output = process.stdout.readline()
+                if output == '' and process.poll() is not None:
+                    break
+                if output:
+                    print(output.strip())
+                    if error_pattern.search(output):
+                        print("Error detected. Restarting the server...")
+                        terminate_process(process)
+                        return True
+            if sys.stdin in ready:
+                input()  # Consume the input to prevent blocking
+    except KeyboardInterrupt:
+        print("Keyboard interrupt received. Terminating the server...")
+        terminate_process(process)
+        return False
     return False
 
 def terminate_process(process):
